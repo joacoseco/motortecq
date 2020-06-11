67afa15883c4ef26e1428297800f576e4d357b42
JoaquinSC <joaquinseco@live.cl>
2020-06-11 15:43:37 -0400

    Creación repositorio GIT

---
+++ motortecq/index.php
@@ -0,0 +1,143 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- FavIcon -->
+    <link rel="icon" href="assets/img/favicon.ico" type="image/ico">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    
+    <!-- FontAwesome -->
+    <script src="https://kit.fontawesome.com/164e1636ad.js" crossorigin="anonymous"></script>
+
+    <style>
+      body{
+        position: relative;
+      }
+    </style>
+    <title>MotorTecq</title>
+  </head>
+  <body data-spy="scroll" data-target=".navbar" data-offset="50">
+    <header>
+      <nav class="navbar navbar-expand-lg bg-dark navbar-dark fixed-top justify-content-center">  
+        <ul class="navbar-nav nav-fill w-50">
+          <li class="nav-item">
+            <a class="nav-link" href="#presentation">INICIO</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#services">SERVICIOS</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#frecuentUsers">CLIENTES FRECUENTES</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#aboutUs">NOSOTROS</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#contact">CONTACTO</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">INICIAR SESIÓN</a>
+          </li>
+        </ul>
+      </nav>
+
+    </header>
+
+    <!-- Section -->
+    <section class="container-fluid" >
+      <div class="d-flex justify-content-center" id="presentation">
+        <div class="text-center" style="margin-top:150px;">
+          <img id="logo1_2" class="rounded mx-auto" src="assets/img/Logo1.png" alt="Logo1.2.png">
+          <h2 align="center">Motortecq - Servicio técnico automotriz</h2>
+        <div>
+      </div>
+
+      <div class="d-flex justify-content-center">
+        <div class="text-center" style="margin-top:250px;">
+          <div id="services" class="carousel slide" data-ride="carousel">
+            <ol class="carousel-indicators">
+              <li data-target="#servicess" data-slide-to="0" class="active"></li>
+              <li data-target="#services" data-slide-to="1"></li>
+              <li data-target="#services" data-slide-to="2"></li>
+              <li data-target="#services" data-slide-to="3"></li>
+            </ol>
+            <div class="carousel-inner">
+              <div class="carousel-item active">
+                <img src="assets/img/Servicios.PNG" class="d-block w-100" alt="Servicios.PNG">
+              </div>
+              <div class="carousel-item">
+                <img src="assets/img/Folleto.PNG" class="d-block w-100" alt="Folleto.PNG">
+              </div>
+              <div class="carousel-item">
+                <img src="assets/img/Elevador.png" class="d-block w-100" alt="Elevador.PNG">
+                <div class="carousel-caption d-none d-md-block">
+                  <h5>Revisión de distintos vehículos</h5>
+                </div>
+              </div>
+              <div class="carousel-item">
+                <div class="carousel-caption d-none d-md-block">
+                  <h5>Revisión de frenos</h5>
+                </div>
+                <img src="assets/img/Frenos.png" class="d-block w-100" alt="Frenos.PNG">
+              </div>
+            </div>
+            <a class="carousel-control-prev" href="#services" role="button" data-slide="prev">
+              <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+              <span class="sr-only">Previous</span>
+            </a>
+            <a class="carousel-control-next" href="#services" role="button" data-slide="next">
+              <span class="carousel-control-next-icon" aria-hidden="true"></span>
+              <span class="sr-only">Next</span>
+            </a>
+          </div>
+        </div>
+      </div>
+
+      <div class="d-flex justify-content-center">
+        <div class="text-center" style="margin-top:250px;">
+          <div id="frecuentUsers" class="carousel slide " data-ride="carousel" style="margin-top:250px;">
+            <ol class="carousel-indicators">
+              <li data-target="#frecuentUsers" data-slide-to="0" class="active"></li>
+              <li data-target="#frecuentUsers" data-slide-to="1"></li>
+            </ol>
+            <div class="carousel-inner">
+              <div class="carousel-item active">
+                <img src="assets/img/Clientes.png" class="d-block w-100" alt="Clientes.png">
+              </div>
+              <div class="carousel-item">
+                <img src="assets/img/LePUCV.PNG" class="d-block w-100" alt="LePUCV.PNG">
+              </div>
+            </div>
+            <a class="carousel-control-prev" href="#frecuentUsers" role="button" data-slide="prev">
+              <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+              <span class="sr-only">Previous</span>
+            </a>
+            <a class="carousel-control-next" href="#frecuentUsers" role="button" data-slide="next">
+              <span class="carousel-control-next-icon" aria-hidden="true"></span>
+              <span class="sr-only">Next</span>
+            </a>
+          </div>
+          <h2 align="center">Clientes frecuentes de MotorTecq</h2>
+        </div>
+      </div>
+    </section>
+
+    <footer></footer>
+
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.js"
+      integrity="sha256-DrT5NfxfbHvMHux31Lkhxg42LY6of8TaYyK50jnxRnM="
+      crossorigin="anonymous">
+    </script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>  
+    
+    <!-- CSS -->
+    <link rel="stylesheet" href="assets/css/main.css">
+  </body>
+</html>
